a41f3ab3acecc69aca186ff6fc74a9b53dd0982e
Giovane Barcelos <giovanebarcelos@gmail.com>
2025-05-30 20:47:48 -0300

    feat: tela principal e menu

--- src/main/java/GerenciadorProjetosApp.java
+++ src/main/java/GerenciadorProjetosApp.java
@@ -2,21 +2,52 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import models.Usuario;
+import org.hibernate.boot.archive.internal.JarProtocolArchiveDescriptor;
 
-public class GerenciadorProjetosApp {
-    private static EntityManagerFactory emf;
+import javax.swing.*;
+import java.awt.*;
+
+public class GerenciadorProjetosApp extends JFrame {
+    private CardLayout cardLayout;
+    private JPanel mainPanel;
+
+    private final String EMPTY_SCREEN = "EMPTY_SCREEN";
+
+    public GerenciadorProjetosApp(){
+        setTitle("Gerenciador de Projetos");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        this.cardLayout = new CardLayout();
+        this.mainPanel = new JPanel(cardLayout);
+
+        JPanel emptyPanel = new JPanel(new BorderLayout());
+        emptyPanel.add(new JLabel("Bem-vindo! Use o menu para navegar.",
+                                   SwingConstants.CENTER),
+                       BorderLayout.CENTER);
+
+        this.mainPanel.add(emptyPanel, EMPTY_SCREEN);
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("Menu");
+
+        JMenuItem listUserItem = new JMenuItem("Lista Usuários");
+        JMenuItem exitItem = new JMenuItem("Sair");
+
+        menu.add(listUserItem);
+        menu.add(exitItem);
+
+        menuBar.add(menu);
+        setJMenuBar(menuBar);
+
+        add(mainPanel);
+    }
 
     public static void main(String[] args) {
-        emf = Persistence.createEntityManagerFactory("PU");
-        EntityManager em = emf.createEntityManager();
-
-        Usuario usuario =
-                new Usuario(1, "Muttley", "muttley@gmail.com",
-                            "12345");
-
-        em.getTransaction().begin();
-        em.persist(usuario);
-        em.getTransaction().commit();
+        SwingUtilities.invokeLater(() -> {
+            new GerenciadorProjetosApp().setVisible(true);
+        });
 
     }
 }
